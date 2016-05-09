1c570d9cd6e02222ec5c638a936a78d576e254c4
Mandi Wise <hello@mandiwise.com>
2016-05-08 18:38:48 -0600

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# WCYVR 2016 - Custom CSS
+
+A little repo containing the CSS customizations for the WordCamp Vancouver 2016 site. Customizations build upon the WordCamp Base Redux theme.
---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    browserSync = require('browser-sync');
+
+var plumberErrorHandler = {
+   errorHandler: notify.onError({
+      title: 'Gulp',
+      message: 'Error: <%= error.message %>'
+   })
+};
+
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(plumber(plumberErrorHandler))
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./'));
+});
+
+gulp.task('watch', function() {
+   gulp.watch('./sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['watch']);
